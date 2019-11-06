a7348cf153c8356eb2fbedd53db255ca768d1522
Erin  Rose <errose10@gmail.com>
2019-11-06 15:28:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
 grr = " and "
-   if array.count >= 3 grr = ", and "
+   if array.count >= 3 
+    then grr = ", and " 
+   end
 
   array.join(", ").sub(/(, )(?!.*, )/i, grr)
 
